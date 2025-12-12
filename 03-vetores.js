fed25dc8c26c222305186ed6993be6119787d323
Rudinei Weschenfelder <rudinei.ti@gmail.com>
2025-12-11 22:45:52 -0300

    vetores e matrizes

---
+++ 03-vetores.js
@@ -0,0 +1,8 @@
+let carros = ["opala", "gol", "chevette"]
+
+console.log(carros) // exibe todos os elementos do array
+
+let linguagens = ["php", "javascript", "python"]
+let lastElement = linguagens.slice(-1) // pega o último elemento do array
+
+console.log(lastElement)
---
+++ 04-matrizes.js
@@ -0,0 +1,11 @@
+let nomePokemon = ["Pikachu", "Chamander", "Bulbassaur"]
+let timePokemon = [
+    ["Pikachu", "M", 1],
+    ["Chamander", "F", 3]
+]
+
+console.log(nomePokemon.pop()) // exemplo de método
+
+console.log(nomePokemon.length) // exemplo de propriedade
+
+console.log("O pokemon " + timePokemon[1][0] + " é do sexo " + timePokemon[1][1] + " e está no nível " + timePokemon[1][2])
